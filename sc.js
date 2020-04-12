f6cdca6a1dc9d038c49169d276cd3ba769b6e418
talalzeini <56060993+talalzeini@users.noreply.github.com>
2020-04-11 20:01:39 -0700

    finished calculator functionality

--- sc.js
+++ sc.js
@@ -6,3 +6,38 @@ calculator.display.value = calculator.display.value.substring(1,calculator.displ
 calculator.display.value = "-"+ calculator.display.value;
 }
 }
+
+function toDeg(){
+    const rad = document.getElementById('rad');
+    const deg= document.getElementById('deg');
+    const radians = document.getElementById('radians');
+    const degrees = document.getElementById('degrees');
+    const radIcon = document.getElementById('radIcon');
+    const degIcon = document.getElementById('degIcon');
+
+    rad.classList.add('hide');
+    deg.classList.remove('hide');
+    radians.classList.remove('hide');
+    degrees.classList.add('hide');
+
+    radIcon.classList.remove('hide');
+    degIcon.classList.add('hide');
+   
+}
+function toRad(){
+    const rad = document.getElementById('rad');
+    const deg= document.getElementById('deg');
+    const radians = document.getElementById('radians');
+    const degrees = document.getElementById('degrees');
+    const radIcon = document.getElementById('radIcon');
+    const degIcon = document.getElementById('degIcon');
+
+    rad.classList.remove('hide');
+    deg.classList.add('hide');
+
+    radians.classList.add('hide');
+    degrees.classList.remove('hide');
+
+    degIcon.classList.remove('hide');
+    radIcon.classList.add('hide');
+}
